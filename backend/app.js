f9cf8ad093eec5db27370bc47b221a803e5c6ea9
TheophilusKgopa <2456207@students.wits.ac.za>
2024-06-10 12:05:32 +0200

    Fixed Connection issues

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,71 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { db } from "./db/index.js";
+import mysql2 from "mysql2";
 import MainRouter from "./routes/index.js";
 dotenv.config({ path: ".env.local" });
 
-const app = express();
+const dbConfig = process.env.DATABASE_URL || {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+};
 
-// Middleware for parsing JSON and urlencoded data
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-// Serving images
 app.use(
   "/uploads/funding_opportunities/",
   express.static("uploads/funding_opportunities/")
 );
 app.use("/uploads/user_documents/", express.static("uploads/user_documents/"));
 
-// Attach the database to the request object
-app.use((req, res, next) => {
-  req.db = db;
+let dbConnection;
+
+const initializeDbConnection = () => {
+  if (!dbConnection) {
+    dbConnection = mysql2.createConnection(dbConfig);
+    dbConnection.connect((err) => {
+      if (err) {
+        console.error("Error connecting to MySQL database:", err);
+        dbConnection = null;
+      } else {
+        console.log("Connected to MySQL database");
+      }
+    });
+  }
+};
+
+const checkDbConnection = () => new Promise((resolve) => {
+  if (!dbConnection) return resolve(false);
+  dbConnection.ping((err) => {
+    if (err) {
+      console.error("MySQL connection lost:", err);
+      dbConnection = null;
+      resolve(false);
+    } else {
+      resolve(true);
+    }
+  });
+});
+
+app.use(async (req, res, next) => {
+  if (!(await checkDbConnection())) initializeDbConnection();
+  if (dbConnection) req.db = dbConnection;
+  else return next(new Error("Database error"));
   next();
 });
 
-// Routes
 app.use("/", MainRouter);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
-  if (err.message === "Unauthenticated") {
-    res.status(401).json({ error: "Unauthenticated" });
-  } else if (err.message === "Database error") {
-    res.status(500).json({ error: "Database error" });
-  } else if (err.message === "Missing required parameters") {
-    res.status(400).json({ error: "Missing required parameters" });
-  } else {
+  if (err.message === "Unauthenticated") res.status(401).json({ error: "Unauthenticated" });
+  else if (err.message === "Database error") res.status(500).json({ error: "Database error" });
+  else if (err.message === "Missing required parameters") res.status(400).json({ error: "Missing required parameters" });
+  else {
     console.error("Error:", err);
     res.status(500).json({ error: "Internal Server Error" });
   }
